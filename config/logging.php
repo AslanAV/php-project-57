eb39aa2d702d8dabec09a089554a52eb35ff71d5
AslanAV <autlevaslan@yandex.ru>
2022-10-25 23:03:06 +0300

    add Rollbar

--- config/logging.php
+++ config/logging.php
@@ -3,6 +3,7 @@
 use Monolog\Handler\NullHandler;
 use Monolog\Handler\StreamHandler;
 use Monolog\Handler\SyslogUdpHandler;
+use Rollbar\Laravel\MonologHandler;
 
 return [
 
@@ -51,9 +52,16 @@ return [
     */
 
     'channels' => [
+        'rollbar' => [
+            'driver' => 'monolog',
+            'handler' => MonologHandler::class,
+            'access_token' => env('ROLLBAR_TOKEN'),
+            'level' => 'debug',
+        ],
+
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['rollbar','single'],
             'ignore_exceptions' => false,
         ],
 

5700b072b5b949f8cbd2a15a168aa2b64beae35b
AslanAV <autlevaslan@yandex.ru>
2022-10-31 12:25:38 +0300

    add force schema https fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 
-use Illuminate\Support\Facades\App;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
@@ -26,8 +26,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        If(App::environment('production')) {
-            URL::forceSchema('https');
+        if (env('FORCE_HTTPS', false)) {// Default value should be false for local server
+            URL::forceScheme('https');
         }
+        Paginator::useBootstrapFive();
     }
 }

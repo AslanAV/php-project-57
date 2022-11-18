69c675c536a75ad020d989b60f80aa12a7449981
AslanAV <autlevaslan@yandex.ru>
2022-11-19 00:06:25 +0300

    fix migration2

--- database/migrations/2022_11_17_184531_create_label_task_table.php
+++ database/migrations/2022_11_17_184531_create_label_task_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateLabelTaskTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -30,4 +30,4 @@ class CreateLabelTaskTable extends Migration
     {
         Schema::dropIfExists('label_task');
     }
-}
+};

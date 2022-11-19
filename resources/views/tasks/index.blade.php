11bbaf98d8cfeb09fb1530f3031aea9702ba8cd2
AslanAV <autlevaslan@yandex.ru>
2022-11-19 14:42:00 +0300

    delete helpers from views

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -55,10 +55,10 @@
             @foreach($tasks as $task)
                 <tr class="border-b border-dashed text-left">
                     <td>{{ $task->id }}</td>
-                    <td>{{ TaskStatusHelper::getNameStatus($task->status_id) }}</td>
+                    <td>{{ $taskStatuses[$task->status_id] }}</td>
                     <td><a href="{{ route('tasks.show', $task) }}">{{ $task->name }}</a></td>
-                    <td>{{ UserHelper::getNameUser($task->created_by_id) }}</td>
-                    <td>{{ UserHelper::getNameUser($task->assigned_to_id) }}</td>
+                    <td>{{ $users[$task->created_by_id] }}</td>
+                    <td>{{ $users[$task->assigned_to_id] }}</td>
                     <td>{{ $task->created_at }}</td>
                     @auth()
                         <td>

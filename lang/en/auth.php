3776c73829e3eaea1fa290160db8b91c0e7463d6
AslanAV <autlevaslan@yandex.ru>
2022-11-19 21:58:48 +0300

    fix ru auth

--- lang/en/auth.php
+++ lang/en/auth.php
@@ -17,5 +17,5 @@ return [
     'password' => 'The provided password is incorrect.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
     'Register' => 'Register',
-
+    'Log in' => 'Log in',
 ];
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,7 +14,7 @@
 
             <!-- Email Address -->
             <div>
-                <x-input-label for="email" :value="__('auth.Email')" />
+                <x-input-label for="email" :value="__('Email')" />
 
                 <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
 
@@ -49,7 +49,7 @@
                 @endif
 
                 <x-primary-button class="ml-3">
-                    Войти
+                    {{ __('auth.Log in') }}
                 </x-primary-button>
             </div>
         </form>
